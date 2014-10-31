49f55fbc0a312fe9bc45cb1bbc364f7a4b02eb7e
Cord Rehn <jordansg57@gmail.com>
2014-10-31 15:20:24 -0700

    Added ninja resource, not used yet though.

--- FFXIV Nexus Progress/My Project/Resources.Designer.vb
+++ FFXIV Nexus Progress/My Project/Resources.Designer.vb
@@ -103,6 +103,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property nin() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("nin", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property pld() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("pld", resourceCulture)
